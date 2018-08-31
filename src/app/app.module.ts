592452c4678e7e9115939bcd924bab423d7559db
Greg.Wallerus <gwallerus@gmail.com>
2018-08-31 14:22:21 -0500

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MessageBoardComponent } from './message-board/message-board.component';
 import { FootballService } from './football.service';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { FootballService } from './football.service';
     MessageBoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [FootballService],
   bootstrap: [AppComponent]
--- src/app/message-board/message-board.component.ts
+++ src/app/message-board/message-board.component.ts
@@ -7,11 +7,13 @@ import { FootballService } from '../football.service';
   styleUrls: ['./message-board.component.css']
 })
 export class MessageBoardComponent implements OnInit {
- 
+
   todaydate;
   constructor(private football: FootballService) { }
   ngOnInit() {
     this.todaydate = this.football.showTodayDate();
   }
-
+  submitComment(data) {
+    alert("hello");
+  }
 }
